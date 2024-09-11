792c86976613c8c77b4e6b801e1d6110a4bb732b
Mazen Chaouch <m.chaouch007@gmail.com>
2024-09-11 19:09:40 +0100

    fix corrupted import

--- components/ui/input-otp.tsx
+++ components/ui/input-otp.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 import { OTPInput, OTPInputContext } from "input-otp";
 import { Dot } from "lucide-react";
 
-import { cn } from "@/lib copy/utils";
+import { cn } from "@/lib/utils";
 
 const InputOTP = React.forwardRef<
   React.ElementRef<typeof OTPInput>,
@@ -51,7 +51,7 @@ const InputOTPSlot = React.forwardRef<
       {char}
       {hasFakeCaret && (
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
-          <div className="h-4 w-px animate-caret-blink bg-slate-950 duration-1000 dark:bg-slate-50" />
+          <div className="animate-caret-blink h-4 w-px bg-slate-950 duration-1000 dark:bg-slate-50" />
         </div>
       )}
     </div>
